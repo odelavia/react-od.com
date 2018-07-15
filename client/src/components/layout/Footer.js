b081bb46188d5b7f17f807f1b5993bd240f41a02
odelavia <odelavia@gmail.com>
2018-07-15 16:23:04 -0400

    Set up footer, NavBar Responsiveness.

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const Footer = () => (
-  <div>
-    <div>FOOTER</div>
-  </div>
+  <footer className="footer ">
+    <div className="container">
+      <nav className="pull-left">
+        <ul>
+          <li>
+            <a href="https://www.oliverdelaviadev.com">
+              De La Via Studio
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              More About Me
+            </a>
+          </li>
+          <li>
+            <a href="https://medium.com/@oliverdelavia">
+              Blog
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="copyright pull-right">
+        &copy;
+        2018, made with  <i className="material-icons">favorite</i>   by
+        <a href="https://www.oliverdelaviadev.com" target="_blank"> De La Via Studio</a>
+        for a better web.
+      </div>
+    </div>
+  </footer>
 
 );
 
