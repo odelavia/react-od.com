74894d21224f2b75dddabf78fd87a5d4f3741746
odelavia <odelavia@gmail.com>
2018-07-18 11:33:48 -0400

    started working on mobile nav funcitonality, set up methods

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navOpen: false,
+      right: 0,
+    }
+    this.openNav = this.navOpen.bind(this)
+  }
+
+  openNav() {
+    !this.state.navOpen ? this.setState({navOpen: true}) : this.setState({navOpen: false})
+    console.log(this.state.navOpen)
+
+    if (this.state.navOpen == true) {
+      // set nav ul.navbar-collapse to right: 300px
+      this.setState({
+        right: 300px
+      })
+    } else {
+      //set nav ul.navbar-collapse to right: 0px
+      right: 0
+    }
+    console.log(this.state.right)
+  };
+
+  // resetMobileNav() {
+  //   navOpen ? (!this.state.linkClicked ? this.setState({linkClicked: true}) : null) : null
+  // };
+
   render() {
     return (
       <nav className="navbar-color-on-scroll navbar-transparent" color-on-scroll="100" id="sectionsNav">
         <div className="navbar-container">
           <div className="navbar-translate">
             <Link className="navbar-brand" to="/">DE LA VIA</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <button onClick={this.openNav} className="navbar-toggler" style={{right: `${this.state.right} px` }} type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
               <span className="navbar-toggler-icon"></span>
               <span className="navbar-toggler-icon"></span>
